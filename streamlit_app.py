bfbaa91d1138da2fe968c06be7d1bcd43a87eb2a
Valtteri Halla <valtteri.halla@gmail.com>
2024-02-23 19:56:32 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-from fastcore.all import *
 from fastai.vision.all import *
 
 df = pd.DataFrame({
