984bbeeeba3fc29c9b7436d35f2171dda81772ae
Valtteri Halla <valtterihalla@users.noreply.github.com>
2024-02-23 18:39:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,14 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+from fastai.vision.all import *
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
 df
+
+nn = load_learner('checkpoint.pkl')
+nn.predict('test.jpg')[0]
