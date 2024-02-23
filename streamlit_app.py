75c060ff38f19dff118240fc03fcfb1398b3f2d9
Valtteri Halla <valtteri.halla@gmail.com>
2024-02-23 20:28:04 +0200

    add image upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,23 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
-import pandas as pd
 from fastai.vision.all import *
+from PIL import Image
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
 
 nn = load_learner('checkpoint.pkl')
-nn.predict('test.jpg')[0]
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+st.sidebar.write("## Upload and download :gear:")
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+col1, col2 = st.columns(2)
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        image = Image.open(my_upload)
+        col1.image(image)
+        nn.predict(image)[0]
+else:
+    nn.predict('test.jpg')[0]
