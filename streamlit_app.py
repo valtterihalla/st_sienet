0c1b5f74796d0fe8be27b420332dd79767dae0dd
Valtteri Halla <valtteri.halla@gmail.com>
2024-02-23 19:47:48 +0200

    add fastcore dependency

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+from fastcore.all import *
 from fastai.vision.all import *
 
 df = pd.DataFrame({
